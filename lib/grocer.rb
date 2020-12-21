4e20219d59d4bde5d64267b38af2589f00831ea4
Erik Huang <erikhuangdrums@gmail.com>
2020-12-21 12:39:55 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do |hash|
     hash.each { |k, v| return hash if hash[:item] == name }
   end
-
+  nil
 end
 
 def consolidate_cart(cart)
