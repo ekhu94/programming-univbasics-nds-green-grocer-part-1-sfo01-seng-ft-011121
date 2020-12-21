f15d2435c3024fa20c52c2759a1f8534d768af0e
Erik Huang <erikhuangdrums@gmail.com>
2020-12-21 12:39:26 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,6 @@
 def find_item_by_name_in_collection(name, collection)
   collection.each do |hash|
-    hash.each { |k, v| hash if v == name }
+    hash.each { |k, v| return hash if hash[:item] == name }
   end
 
 end
